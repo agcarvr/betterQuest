c0800475067ad366f7354fcc4227df06d1a3c167
aaroncarver <agcarvr@gmail.com>
2021-04-24 09:32:01 -0600

    Made user details page

--- src/Compontents/UserDetails/UserDetails.js
+++ src/Compontents/UserDetails/UserDetails.js
@@ -1,7 +1,16 @@
-export default function User() {
+export default function UserDetails({ data }) {
     return(
         <div>
-            <h1>this is a user profile</h1>
+
+            <h1>Hey There, {data.users.username}!</h1>
+            
+            <h3><u>Here are your current stats:</u></h3>
+            
+            <p><b>Strength:</b> {data.users.strength}</p>
+            <p><b>Endurance:</b> {data.users.endurance}</p>
+            <p><b>Wisdom:</b> {data.users.wisdom}</p>
+            
+            <h3>I wonder how much you will improve today!</h3>
             
         </div>
     )
