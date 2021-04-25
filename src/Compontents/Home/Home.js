b6529dcd889b4f964c368cbfd4dc3cfe139091ba
aperezdom <aideperz@gmail.com>
2021-04-25 10:03:46 -0600

    got my profile and quests centered

--- src/Compontents/Home/Home.js
+++ src/Compontents/Home/Home.js
@@ -9,7 +9,7 @@ export default function Home (){
             <div className='mainlogo'>
             <img src="https://i.imgur.com/OF63QJV.png" width="200" height="200" className="app-logo" alt="betterQuest-app-logo" />
             </div>
-            <div>
+            <div className='home-body'>
             <h1 className="logo-app-name">~ Welcome to your Real Life State Tracker ~</h1>
             <Link to='/userdetails'  className='user-link'><div>Profile</div></Link>
             <Link to='/quests' className='quests-link'> <div>Quests</div></Link>
