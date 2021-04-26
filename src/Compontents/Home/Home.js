833b8a8e22d8541fdb74f988cb6032ed02fbeeb9
aperezdom <aideperz@gmail.com>
2021-04-26 09:52:13 -0600

    Added theme song for home

--- src/Compontents/Home/Home.js
+++ src/Compontents/Home/Home.js
@@ -1,10 +1,17 @@
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import Sound from 'react-sound';
+import TownTheme from '../../sounds/TownTheme.mp3';
 import styles from './Home.module.css';
 
 
 export default function Home (){
     return(
         <div>
+            <Sound
+            url={TownTheme}
+            playStatus={Sound.status.PLAYING}>
+
+            </Sound>
            
             <div className={styles.mainlogo}>
             {/* <img src="https://i.imgur.com/OF63QJV.png" width="200" height="200" className="app-logo" alt="betterQuest-app-logo" /> */}
