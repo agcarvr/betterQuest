ac6c43ef5222541a2723b21ab604b25ce4426649
aperezdom <aideperz@gmail.com>
2021-04-25 22:42:20 -0600

    user detail components looking good so far:

--- src/Compontents/Nav/Nav.js
+++ src/Compontents/Nav/Nav.js
@@ -6,7 +6,7 @@ export default function Nav (){
     return(
         <div className={styles.navbar}>
             <Link to='/' className={styles.main}>
-                <div className='header' >
+                <div className={styles.header} >
                     <img src='https://i.imgur.com/OF63QJV.png' width="200" height="200" className="app-logo" alt="betterQuest-app-logo" ></img>
                 </div>
             </Link>
--- src/Compontents/UserDetails/UserDetails.js
+++ src/Compontents/UserDetails/UserDetails.js
@@ -5,17 +5,17 @@ export default function UserDetails({ data }) {
     return(
 
         <div>
-             <img src="https://i.imgur.com/OF63QJV.png" width="200" height="200" className="app-logo" alt="betterQuest-app-logo" />
-            <h1 className='hey-there'>|~ Hey There, {data.users.username}! ~|</h1>
+             {/* <img src="https://i.imgur.com/OF63QJV.png" width="200" height="200" className="app-logo" alt="betterQuest-app-logo" /> */}
+            <h1 className={styles.userheader}>|~ Hey There, {data.users.username}! ~|</h1>
             
             <h3><u className={styles.UserDetails}>Here are your current stats:</u></h3>
             
-            <p><b>Strength:</b> {data.users.strength}</p>
-            <p><b>Endurance:</b> {data.users.endurance}</p>
-            <p><b>Wisdom:</b> {data.users.wisdom}</p>
-            
-            <Link to="/quests"><h3>How will you better yourself today?</h3></Link>
-            
+            <p className={styles.UserGains}><b>Strength:</b> {data.users.strength}</p>
+            <p className={styles.UserGains}><b>Endurance:</b> {data.users.endurance}</p>
+            <p className={styles.UserGains}><b>Wisdom:</b> {data.users.wisdom}</p>
+            <div className={styles.linkdivhome}>
+            <Link to="/quests"><h3 className={styles.questlink}>Better Yourself Here?</h3></Link>
+            </div>
         </div>
     )
 }
