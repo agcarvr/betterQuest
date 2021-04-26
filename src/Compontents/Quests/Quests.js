9ea733b0090fd201de8fca5ab2f7ef70b37f8c79
aaroncarver <agcarvr@gmail.com>
2021-04-26 16:22:13 -0600

    fixed styles merge conflict

--- src/Compontents/Quests/Quests.js
+++ src/Compontents/Quests/Quests.js
@@ -1,10 +1,6 @@
 import { useState } from 'react';
 import {Link} from 'react-router-dom'
-<<<<<<< HEAD
-import styles from './Quests.modules.css';
-=======
 import styles from './Quests.module.css';
->>>>>>> 8eb60008c82b7862a1f0b014ec9c32cd9b620c89
 
 
 export default function Quest({data}){
